c822bbb4bf8859516bb1058426807b41f5f0b67e
ThierryCohnen <67508773+ThierryCohnen@users.noreply.github.com>
2021-04-15 16:37:55 +0200

    added interest pictures and gifs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import NikonPic from './pexels-photo-1250282.jpg';
 import './App.css';
 
 function App() {
@@ -18,6 +19,14 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <br></br>
+      <div>
+        <a href="https://www.logosurfer.com/wp-content/uploads/2018/03/manchester-united-logo_0.jpg" target="_blank"><img width="240" height="135" src="https://www.logosurfer.com/wp-content/uploads/2018/03/manchester-united-logo_0.jpg" alt="Manchester United Logo" /></a>&nbsp;
+        <iframe src="https://giphy.com/embed/1yLEsc4IF2Akm4InqA" width="240" height="135" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><a href="https://giphy.com/gifs/officialWRC-rally-dust-mexico-1yLEsc4IF2Akm4InqA"></a>&nbsp;
+        <a><img width="240" height="135" src={NikonPic} /></a>
+
+      </div>
     </div>
   );
 }
